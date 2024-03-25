a8c6e506ab32601bc69d7b8758da72e83fd429bd
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2024-03-25 22:24:15 +0100

    login

--- src/navbar/navbar.php
+++ src/navbar/navbar.php
@@ -46,7 +46,7 @@
                     <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Sign out</a>
                     </div> -->
                 </div>
-            </li>
+            </li> 
             <li>
         <button id="dropdownNavbarLinkSetting" data-dropdown-toggle="dropdownNavbarSetting" class="text-white hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0 font-medium flex items-center justify-between w-full md:w-auto">Setting <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
                 <!-- Dropdown menu -->
